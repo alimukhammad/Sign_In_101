5f713eacaebb083db8b19949b1f60b25dfeecb52
sardor <alimukhammad1@gmail.com>
2024-06-26 00:32:49 -0400

    1

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 let username = document.getElementById('username');
 let password = document.getElementById('password');
 let submit = document.getElementById('loadButton');
+let load = document.getElementById('loader');
+
+submit.addEventListener('click', function() {
+    event.preventDefault(); // Prevent the default form submit action
+    console.log("Button clicked")
+    document.getElementById('loader').style.display = 'block';
+    console.log("Username: " + username.value);
+    let time = 10;
+    for(let i = 0; i < time; i++) {
+        console.log("Loading..." + i + "%");
+    } 
+});
